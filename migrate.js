7c37192fa33eb010a4f625de1dd9d9cf5c1d1469
Pavan Karthick <mpkarthick2002@gmail.com>
2023-01-31 01:40:15 +0530

    Code added

---
+++ migrate.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+async function copyCollections() {
+    
+    var db1 = "NOT THAT DUMB";
+    var db2 = "NOT THAT DUMB xD";
+    const client1 = await MongoClient.connect(db1, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client2 = await MongoClient.connect(db2, { useNewUrlParser: true, useUnifiedTopology: true });
+    db1 = client1.db("src_db");
+    db2 = client2.db("dest_db");
+    const collections = await db1.listCollections().toArray();
+    for (let i = 0; i < collections.length; i++) {
+        const collection = collections[i];
+        const name = collection.name;
+        const options = collection.options;
+        await db2.createCollection(name, options);
+        const cursor = await db1.collection(name).find();
+        while (await cursor.hasNext()) {
+            const doc = await cursor.next();
+            await db2.collection(name).insertOne(doc);
+        }
+    }
+    client1.close();
+    client2.close();
+}
+
+copyCollections();
